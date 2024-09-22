d6cdaf77db600586894cdb2f2cd6aa8fa1ce5a5d
Sawyer Redstone <47250827+SawyerRedstone@users.noreply.github.com>
2024-09-22 17:32:46 -0400

    Update sorted_squares.py
    Changing complexity analysis to use lowercase n for consistency.

--- sorted_squares.py
+++ sorted_squares.py
@@ -6,8 +6,8 @@ class Solution:
         After squaring, the left and rightmost sides will be largest.
         Using two pointers, we can keep adding the larger one to the the solution list.
 
-        Time: O(N) - Together, the left and right pointers traverse the entire list of size n. 
-        Space: O(N) if the output list is considered, O(1) otherwise.
+        Time: O(n) - Together, the left and right pointers traverse the entire list of size n. 
+        Space: O(n) if the output list is considered, O(1) otherwise.
         """
         # Make an empty list to fill with sorted values.
         result = [None] * len(nums)
